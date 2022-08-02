56b39a745b4a549e3e84a6bca828d6aebbf3a779
indexpz <pawelz72@gmail.com>
2022-08-02 21:26:48 +0200

    Dicom are shown

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ print(images[0].split('.'))
 
 for img in images:
     new_file_name = img.split('.')[0]
-    new_file_name += '.jpg'
+    # new_file_name += '.jpg'
     print(new_file_name)
     image = pydicom.dcmread(img)
     image = image.pixel_array.astype(float)
@@ -16,7 +16,9 @@ for img in images:
     rescaled_image = (np.maximum(image, 0) / image.max()) * 255  # float pixels
     final_image = np.int8(rescaled_image)  # integer pixels
     final_image = Image.fromarray(final_image)
+    # final_image.save('ala.jpg')
     final_image.show()
-    final_image.save("test.jpg")
-    # final_image = open(new_file_name, "w")
-    # final_image.close()
+
+
+
+
